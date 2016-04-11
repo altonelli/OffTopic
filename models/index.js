deb4ac1e9b22ed37b546ae3da58bd11f3f4bd03b
altonelli <altonelli14@gmail.com>
2016-04-10 22:29:32 -0700

    Deploying intial beta

--- models/index.js
+++ models/index.js
@@ -1,5 +1,10 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/offtopic");
+// mongoose.connect("mongodb://localhost/offtopic");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "https://fast-cliffs-93554.herokuapp.com/" );
+
+// https://fast-cliffs-93554.herokuapp.com/ | https://git.heroku.com/fast-cliffs-93554.git
 
 module.exports.Post = require("./post.js");
 module.exports.Comment = require("./comment.js");
